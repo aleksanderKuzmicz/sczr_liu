3d82bad2461844e5e276040c89f9d9edfe998972
alex <aleksander.kuzmicz1@gmail.com>
2023-01-04 19:54:29 +0100

    classes

--- src/gui.py
+++ src/gui.py
@@ -1,30 +1,32 @@
-import sys
 
-from src.configuration.config import WINDOW_SIZE, COLOR_PALETTE, BUTTON_SIZE, LINE_EDIT_SIZE
+from src.configuration.config import WINDOW_HEIGHT, WINDOW_WIDTH, SCROLL_HEIGHT, SCROLL_WIDTH, BUTTON_WIDTH, \
+    BUTTON_HEIGHT, LINE_EDIT_SIZE, APP_BG_COLOR
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
-    QApplication,
     QMainWindow,
     QWidget,
     QLineEdit,
     QPushButton,
     QLabel,
+    QScrollArea,
     QVBoxLayout,
     QGridLayout,
     QFormLayout,
+    QFrame
 )
 
 
 class LiuWindow(QMainWindow):
     """PyCalc's main window (GUI or view)."""
+    # tasks_list = None
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("App")
-        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.general_layout = QGridLayout()
         central_widget = QWidget(self)
-
+        central_widget.setStyleSheet(f"background-color: {APP_BG_COLOR}")
         central_widget.setLayout(self.general_layout)
         self.setCentralWidget(central_widget)
 
@@ -44,85 +46,117 @@ class LiuWindow(QMainWindow):
 
         # layout for user's input
         user_input_layout = QFormLayout()
-        user_input_layout.addRow("Minimum time:", self.user_min_time)
-        user_input_layout.addRow("Maximum time:", self.user_max_time)
-        user_input_layout.addRow("Tasks number:", self.user_tasks_number)
+        user_input_layout.addRow("<p style=\"font-size:15px\">Minimum time:</p>", self.user_min_time)
+        user_input_layout.addRow("<p style=\"font-size:15px\">Maximum time:</p>", self.user_max_time)
+        user_input_layout.addRow("<p style=\"font-size:15px\">Tasks number:</p>", self.user_tasks_number)
         user_input_widget = QWidget()
         user_input_widget.setLayout(user_input_layout)
 
         # buttons
-        self.button_fill_input = QPushButton("Fill input values table")
-        self.button_fill_input.setFixedWidth(BUTTON_SIZE)
+        self.button_clear = QPushButton("Clear all")
+        self.button_user = QPushButton("Create tasks")
+        self.button_default = QPushButton("Create default tasks")
+        self.button_run_alg = QPushButton("Order tasks")
+
+        self.button_clear.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        self.button_user.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        self.button_default.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        self.button_run_alg.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        self.button_clear.setStyleSheet(
+            "font-size:15px; font-weight:bold; border-radius : 5; background-color: #DE3163; color:white; border-bottom: 1px solid grey; border-right:1px solid grey;")
+        self.button_user.setStyleSheet(
+            "font-size:15px; border-radius : 5; background-color: #cfeafb; color:black; border-bottom: 1px solid grey; border-right:1px solid grey;")
+        self.button_default.setStyleSheet(
+            "font-size:15px; border-radius : 5; background-color: #cfeafb; color:black; border-bottom: 1px solid grey; border-right:1px solid grey;")
+        self.button_run_alg.setStyleSheet(
+            "font-size:15px; border-radius : 5; background-color: #cfeafb; color:black; border-bottom: 1px solid grey; border-right:1px solid grey;")
+
+        # invalid params label
+        self.invalid_params_label = QLabel("")
+        self.invalid_params_label.setStyleSheet("font-size:15px; color:red")
 
         # add user input layout to user quarter layout
         self.user_quarter_layout = QVBoxLayout()
         self.user_quarter_layout.addWidget(QLabel("<h1>Algorithm Liu</h1>"), alignment=Qt.AlignmentFlag.AlignCenter)
-        self.user_quarter_layout.addWidget(QLabel("<h3>TODO: add Graham's notation</h1>"), alignment=Qt.AlignmentFlag.AlignCenter)
+        self.user_quarter_layout.addWidget(QLabel("<h2>1 | r<sub>i</sub>, prm | L<sub>max</sub></h2>"),
+                                           alignment=Qt.AlignmentFlag.AlignCenter)
         self.user_quarter_layout.addWidget(user_input_widget, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.user_quarter_layout.addWidget(self.button_fill_input, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.user_quarter_layout.addWidget(self.button_user, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.user_quarter_layout.addWidget(self.button_default, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.user_quarter_layout.addWidget(self.button_run_alg, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.user_quarter_layout.addWidget(self.button_clear, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.user_quarter_layout.addWidget(self.invalid_params_label, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # add user's layout to user widget
-        # add user widget to general layout
-        # ####user_widget = QWidget()
-        # ####user_widget.setLayout(self.user_quarter_layout)
-        # ####self.general_layout.addWidget(user_widget, 0, 0)
         self.general_layout.addLayout(self.user_quarter_layout, 0, 0, alignment=Qt.AlignmentFlag.AlignCenter)
 
     def _set_input_quarter(self):
         # input values layout
-        input_values_widget = QWidget()
         self.input_values_layout = QGridLayout()
+        self.input_values_layout.setSpacing(1)
+        input_values_widget = QWidget()
         input_values_widget.setLayout(self.input_values_layout)
 
         # add input values layout to input quarter layout
         self.input_quarter_layout = QVBoxLayout()
         self.input_quarter_layout.addWidget(QLabel("<h2>Input values</h2>"), alignment=Qt.AlignmentFlag.AlignCenter)
         self.input_quarter_layout.addWidget(input_values_widget, alignment=Qt.AlignmentFlag.AlignCenter)
-        # TODO: delete below lines
-        self.foo_button = QPushButton("foo")
-
-        # self.user_quarter_layout.addLayout(self.input_values_layout)
 
         # add input layout to input widget
-        # add input widget to general layout
-        # #### input_widget = QWidget()
-        # #### input_widget.setLayout(self.input_quarter_layout)
-        # #### self.general_layout.addWidget(input_widget, 1, 0)
         self.general_layout.addLayout(self.input_quarter_layout, 1, 0)
-        self.general_layout.addWidget(self.foo_button, 3, 0)
 
     def _set_output_quarter(self):
-        pass
+        # input values layout
+        self.output_values_layout = QGridLayout()
+        self.output_values_layout.setSpacing(1)
+        output_values_widget = QWidget()
+        output_values_widget.setLayout(self.output_values_layout)
+
+        # add output values layout to output quarter layout
+        self.output_quarter_layout = QVBoxLayout()
+        self.output_quarter_layout.addWidget(QLabel("<h2>Output values</h2>"), alignment=Qt.AlignmentFlag.AlignCenter)
+        self.output_quarter_layout.addWidget(output_values_widget, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.general_layout.addLayout(self.output_quarter_layout, 0, 1)
 
     def _set_graph_quarter(self):
-        pass
+        self.graph_values_layout = QGridLayout()
+        self.graph_values_layout.setSpacing(1)
+        graph_values_widget = QWidget()
+        graph_values_widget.setLayout(self.graph_values_layout)
+        # scroll
+        graph_scroll = QScrollArea()
+        graph_scroll.setWidgetResizable(True)
+        graph_scroll.setFixedSize(SCROLL_WIDTH, SCROLL_HEIGHT)
+        graph_scroll.setFrameShape(QFrame.Shape.NoFrame)
+        graph_scroll.setWidget(graph_values_widget)
 
-    def return_user_values(self) -> dict:
+        # Lmax
+        self.lmax_label = QLabel("")
+        # self.lmax_label.setStyleSheet("font-size:15px")
+
+
+        self.graph_quarter_layout = QVBoxLayout()
+        self.graph_quarter_layout.addWidget(QLabel("<h2>Graph</h2>"), alignment=Qt.AlignmentFlag.AlignCenter)
+        self.graph_quarter_layout.addWidget(graph_scroll, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.graph_quarter_layout.addWidget(self.lmax_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.general_layout.addLayout(self.graph_quarter_layout, 1, 1)
+
+    def _clear_user_values(self):
+        print("[Start] _clear_user_values")
+        self.user_min_time.setText("")
+        self.user_max_time.setText("")
+        self.user_tasks_number.setText("")
+        self.user_min_time.setFocus()
+        print("[End]   _clear_user_values")
+
+    def get_user_values(self) -> dict:
+        print("[Start] get_user_values")
         user_values = {
-            "min_time": self.user_min_time,
-            "max_time": self.user_max_time,
-            "tasks_number": self.user_tasks_number
+            "min_time": self.user_min_time.text(),
+            "max_time": self.user_max_time.text(),
+            "tasks_number": self.user_tasks_number.text()
         }
+        print(f"[End] get_user_values, returning: {user_values}")
         return user_values
-
-    def fill_input_quarter(self, tasks_list):
-        for row, task in enumerate(tasks_list):
-            color = COLOR_PALETTE[row]
-            for col, task_parameter in enumerate(task):
-                parameter = QLabel(str(task_parameter))
-                parameter.setStyleSheet(f"background-color: {color}")
-                self.input_values_layout.addWidget(parameter, row, col)
-            print(f"color: {color}")
-        super().show()
-
-
-def main():
-    """PyCalc's main function."""
-    lui_app = QApplication([])
-    lui_window = LiuWindow()
-    lui_window.show()
-    sys.exit(lui_app.exec())
-
-
-if __name__ == "__main__":
-    main()
